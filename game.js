040a60a0ce5a0b22c886bf66eb419ad111ea6f0f
George geham <george25gehamnew@gmail.com>
2024-05-12 14:54:55 +0300

    Auto play added

--- game.js
+++ game.js
@@ -12,20 +12,24 @@ const game = {
         beats: "paper"
     }
 };
-let wins = 0;
-let loses = 0;
-let ties = 0;
+const result = JSON.parse(localStorage.getItem('result')) || {
+    wins: 0,
+    loses: 0,
+    ties: 0
+};
+displayResult();
 function play(move) {
     const computermove = Object.keys(game)[Math.floor(Math.random() * 10 % 3)];
     console.log(move);
     console.log(game[computermove]['loses']);
     if (move === game[computermove]['loses'])
-        wins++;
-    else if (move === game[computermove]['loses'])
-        ties++;
+        result.wins++;
+    else if (move === computermove)
+        result.ties++;
     else
-        loses++;
+        result.loses++;
     displayResult();
+    save();
 }
 function listen() {
     document.querySelectorAll(".icon").forEach(button => {
@@ -36,6 +40,38 @@ function listen() {
     })
 };
 function displayResult() {
-    document.querySelector(".result").innerHTML = `Wins : ${wins} , Loses : ${loses} , Ties : ${ties}`;
+    document.querySelector(".result").innerHTML = `Wins : ${result.wins} , Loses : ${result.loses} , Ties : ${result.ties}`;
 }
+function save() {
+    localStorage.setItem('result', JSON.stringify(result));
+}
+document.querySelector(".reset").addEventListener('click', () => {
+    result.wins = 0;
+    result.loses = 0;
+    result.ties = 0;
+    displayResult();
+    save();
+})
+let isauto = false;
+let key;
+function autoplay() {
+    console.log(isauto);
+    if (!isauto) {
+        isauto = true;
+        key = setInterval(() => {
+            const auto = Object.keys(game)[Math.floor(Math.random() * 5 % 3)];
+            play(auto);
+        }, 1000);
+        document.querySelector('.autoPlay').innerHTML = 'Stop';
+    }
+    else {
+        clearInterval(key);
+        isauto = false;
+        document.querySelector('.autoPlay').innerHTML = 'Auto Play';
+    }
+}
+document.querySelector(".autoPlay").addEventListener('click', () => {
+    console.log("Pressed");
+    autoplay();
+});
 listen();
